6b3b22579cfcc824f01eb68b9a32aef9021ae18c
Eglo68 <damanikvavian@gmail.com>
2024-11-13 07:56:29 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Siapa namamu?");
+var nama = Console.ReadLine();
+
+Console.WriteLine("dimana rumahmu?");
+var rumah = Console.ReadLine();
+
+Console.WriteLine("apa impianmu?");
+var impian = Console.ReadLine();
+
+Console.WriteLine("nama saya adalah" + nama);
+Console.WriteLine("alamat rumah saya di" + rumah);
+Console.WriteLine("impian saya menjadi" + impian);
